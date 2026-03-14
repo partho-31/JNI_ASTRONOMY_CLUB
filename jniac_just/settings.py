1362a4b92214e3403834c2cd39772cd544618e38
partho-31 <parthoemperors@gmail.com>
2026-03-14 15:40:50 +0600

    Debug toolar has been removed

--- jniac_just/settings.py
+++ jniac_just/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'magazines',
     'users',
     'api',
-    "debug_toolbar",
+    # "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'jniac_just.urls'
@@ -207,8 +207,8 @@ CORS_ALLOWED_ORIGINS = [
     
 ]
 
-INTERNAL_IPS = [
-    # ...
-    "127.0.0.1",
-    # ...
-]
+# INTERNAL_IPS = [
+#     # ...
+#     "127.0.0.1",
+#     # ...
+# ]
--- jniac_just/urls.py
+++ jniac_just/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from debug_toolbar.toolbar import debug_toolbar_urls
+# from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 schema_view = get_schema_view(
@@ -27,5 +27,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-] + debug_toolbar_urls()
+] 
+# + debug_toolbar_urls()
 
